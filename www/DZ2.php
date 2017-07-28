ce3ed0b14909c76c6b7c41585a691b6a795bf274
Andrei Timotin <andreitimotin11@gmail.com>
2017-07-28 18:30:36 +0300

    testing

--- www/DZ2.php
+++ www/DZ2.php
@@ -8,8 +8,14 @@
 /*
 2. Решите задачу загрузки файла от пользователя в заданное место на сервере с тем же именем файла, что он имел на компьютере пользователя.
  */
+if (isset($_FILES['file'])) {
+//	echo 1;
+	var_dump($_FILES['file']);
+	$name = $_FILES['file']['name'];
+		move_uploaded_file($_FILES['file']['tmp_name'], $name);
+}
 ?>
 <form enctype="multipart/form-data" method="post" action="DZ2.php">
-	<input type="file" name="file">
-	<button type="submit"> GO!</button>
+    <input type="file" name="file">
+    <button type="submit"> GO!</button>
 </form>
